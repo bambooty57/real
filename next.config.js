4de34c5cc170cd1b52bce481b3a89689de852d3d
Admin <admin@example.com>
2025-01-27 21:15:34 +0900

    feat: API 라우트의 파일 크기 제한을 10MB로 상향 조정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    },
+    responseLimit: '10mb'
+  },
   images: {
     domains: ['firebasestorage.googleapis.com', 't1.daumcdn.net', 'dapi.kakao.com']
   },
